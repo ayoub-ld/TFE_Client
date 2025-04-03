2aa7e4463d5c76885bc0e903cbe50e35297ce39c
Ayoub <lamkadam.ab@gmail.com>
2025-04-03 14:24:28 +0200

    updated page.tsx when connected, created logged-out page, creater Post component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import Image from "next/image";
-import LoginPage from "./login/page";
+import Post from "@/components/post/page";
 
 export default function Home() {
   return (
     <>
-      <LoginPage />
+      <div className="flex flex-col items-center justify-center w-220 min-h-screen py-2 bg-gray-600 border-2 border-gray-800 rounded-2xl">
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
     </>
   );
 }
---
+++ src/components/post/page.tsx
@@ -0,0 +1,14 @@
+export default function Post() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-85 w-200 py-2 bg-gray-800 border-2 border-gray-800 rounded-2xl text-white mb-1">
+        <section className="flex flex-col items-start justify-start m-2 p-10 w-full">
+          <p>Placeholder for a post</p>
+        </section>
+        <section className="flex flex-col items-end justify-start m-2 p-15 w-full">
+          <p>nb likes</p>
+        </section>
+      </div>
+    </>
+  );
+}
